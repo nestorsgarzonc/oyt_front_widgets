5de57cf4f43c1695f3fedaef3ed1ca8b0688abeb
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-27 11:15:23 -0500

    Modified section title

--- lib/title/section_title.dart
+++ lib/title/section_title.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class SectionTitle extends StatelessWidget {
-  const SectionTitle({Key? key, required this.title}) : super(key: key);
+  const SectionTitle({Key? key, required this.title, this.subtitle}) : super(key: key);
 
   final String title;
+  final String? subtitle;
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +14,13 @@ class SectionTitle extends StatelessWidget {
         const SizedBox(height: 14),
         Text('$title:', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
         const SizedBox(height: 14),
+        if (subtitle != null) ...[
+          Text(
+            subtitle!,
+            style: const TextStyle(fontSize: 15),
+          ),
+          const SizedBox(height: 14),
+        ]
       ],
     );
   }
