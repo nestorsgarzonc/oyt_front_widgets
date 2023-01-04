9642cee1cbb646d4bed7e415a0fb6ef8c2a05b84
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-04 16:15:40 -0500

    Improced layout

--- lib/drawer/drawer_layout.dart
+++ lib/drawer/drawer_layout.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:oyt_front_core/theme/theme.dart';
 
 class DrawerLayout extends StatelessWidget {
-  const DrawerLayout({super.key, required this.drawerChild, required this.bodyChild});
+  const DrawerLayout({
+    super.key,
+    required this.drawerChild,
+    required this.bodyChild,
+    this.drawerWidth = 200,
+  });
 
   final Widget drawerChild;
   final Widget bodyChild;
+  final double drawerWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,7 @@ class DrawerLayout extends StatelessWidget {
         Container(
           decoration: CustomTheme.drawerBoxDecoration,
           padding: const EdgeInsets.all(15),
-          width: 200,
+          width: drawerWidth,
           height: double.infinity,
           child: drawerChild,
         ),
