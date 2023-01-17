b75dd94260fbc6db46f183aff1421ec7ec8552bf
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-17 12:49:36 -0500

    Improved animated background

--- lib/widgets/backgrounds/animated_background.dart
+++ lib/widgets/backgrounds/animated_background.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AnimatedBackground extends StatefulWidget {
@@ -60,62 +61,65 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> {
     final middleHeight = size.height / 2;
 
     final random = Random();
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Theme.of(context).primaryColor,
-                  Colors.redAccent,
-                ],
-              ),
-            ),
-          ),
-          ...List.generate(
-            random.nextInt(10) + 20,
-            (index) => AnimatedPositioned(
-              key: Key('$index'),
-              duration: Duration(milliseconds: 500 + random.nextInt(1000)),
-              curve: Curves.easeInOut,
-              top: _animate ? size.height * random.nextDouble() : middleHeight,
-              left: _animate ? size.width * random.nextDouble() : middleWidth,
-              child: Transform.rotate(
-                angle: 2 * pi * random.nextDouble(),
-                child: FaIcon(
-                  foodIcons.elementAt(random.nextInt(foodIcons.length)),
-                  size: 50 * random.nextDouble() + 20,
-                  color: Colors.white.withOpacity(min(1, random.nextDouble() + 0.6)),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 20 + MediaQuery.of(context).padding.top,
-            left: 20,
-            right: 20,
-            bottom: 20 + MediaQuery.of(context).padding.bottom,
-            child: Container(
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                   colors: [
-                    Colors.white.withOpacity(0.9),
-                    Colors.white.withOpacity(1),
+                    Theme.of(context).primaryColor,
+                    Colors.redAccent,
                   ],
                 ),
-                borderRadius: BorderRadius.circular(20),
               ),
-              child: widget.child,
             ),
-          ),
-        ],
+            ...List.generate(
+              random.nextInt(10) + 20,
+              (index) => AnimatedPositioned(
+                key: Key('$index'),
+                duration: Duration(milliseconds: 500 + random.nextInt(1000)),
+                curve: Curves.easeInOut,
+                top: _animate ? size.height * random.nextDouble() : middleHeight,
+                left: _animate ? size.width * random.nextDouble() : middleWidth,
+                child: Transform.rotate(
+                  angle: 2 * pi * random.nextDouble(),
+                  child: FaIcon(
+                    foodIcons.elementAt(random.nextInt(foodIcons.length)),
+                    size: 50 * random.nextDouble() + 20,
+                    color: Colors.white.withOpacity(min(1, random.nextDouble() + 0.6)),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 20 + MediaQuery.of(context).padding.top,
+              left: 20,
+              right: 20,
+              bottom: 20 + MediaQuery.of(context).padding.bottom,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.white.withOpacity(0.9),
+                      Colors.white.withOpacity(1),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: widget.child,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
