bdadeb403241889333e1c915202e9138724115a1
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-24 21:07:39 -0500

    Improved base bottom sheet

--- lib/bottom_sheet/base_bottom_sheet.dart
+++ lib/bottom_sheet/base_bottom_sheet.dart
@@ -8,7 +8,6 @@ class BaseBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
       padding: const EdgeInsets.all(20),
       child: child,
     );
